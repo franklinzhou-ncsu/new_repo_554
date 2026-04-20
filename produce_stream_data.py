3c17531a06917f4a954b334a875c0bd82ead0878
franklinzhou-ncsu <szhou27@ncsu.edu>
2026-04-19 20:03:42 -0400

    Add files via upload

--- produce_stream_data.py
+++ produce_stream_data.py
@@ -5,6 +5,9 @@ Reads power_streaming_data.csv and repeatedly samples 5 rows,
 writing each sample as a csv file to the stream_folder.
 
 The loop runs 20 iterations with a 10-second pause between each output.
+
+Run with:
+python3 Final_Project/produce_stream_data.py
 """
 
 import time
@@ -15,6 +18,6 @@ df = pd.read_csv("Final_Project/power_streaming_data.csv")
 
 # 20 iterations
 for i in range(20):
-    sample = df.sample(5)
-    sample.to_csv(f"Final_Project/stream_folder/batch_"+ str(i) + ".csv", index = False)
-    time.sleep(2) # Pause for 10 seconds in between outputting of data sets
+    sample = df.sample(5) # Randomly sample five rows
+    sample.to_csv(f"Final_Project/stream_folder/batch_"+ str(i) + ".csv", index = False) # output to a .csv file
+    time.sleep(10) # Pause for 10 seconds in between outputting of data sets
